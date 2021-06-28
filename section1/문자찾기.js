b142d2dfbf6b72431a98ed11bf0898828abc146b
seulgooooo <seulgooooo@kakao.com>
2021-06-28 18:57:46 +0900

    no message

--- section1/문자찾기.js
+++ section1/문자찾기.js
@@ -1,4 +1,6 @@
 
+// str 안의 단어중 U가 몇개인지 찾아내는 프로그램을 작성하시오.
+
 function sol(s, t) {
     answer = 0;
     for(let i of s) {
--- section1/삼각형 판별하기.js
+++ section1/삼각형 판별하기.js
@@ -1,14 +1,14 @@
-function sol(s) {
-    for(let i of s) {
-        if (i <= )
-    }
-    
+//길이가 서로 다른 A, B, C 세 개의 막대 길이가 주어지면 이 세 막대로 
+// 삼각형을 만들 수 있 으면 “YES"를 출력하고, 만들 수 없으면 ”NO"를 출력한다.
+
+function sol(a, b, c){
+    let answer="YES", max;
+    let tot=a+b+c;
+    if(a>b) max=a;
+    else max=b;
+    if(c>max) max=c;
+    if(tot-max<=max) answer="NO"; 
+    return answer;
 }
 
-
-
-
-
-const sir = [6, 7, 11]
-
-console.log(sol(sir));
+console.log(sol(13, 33, 17));
