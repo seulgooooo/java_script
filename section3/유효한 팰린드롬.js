6a1e27c29b12f92dfb787521c209b97440e5d33d
seulgooooo <seulgooooo@kakao.com>
2021-06-29 20:47:28 +0900

    no message

--- section3/유효한 팰린드롬.js
+++ section3/유효한 팰린드롬.js
@@ -2,3 +2,13 @@
 // 단 알파벳 외의 문자를 무시하고 알파벳만 회문인지 구분하라.
 
 
+function sol(s){
+    let answer="YES";
+    s = s.toLowerCase().replace(/[^a-z]/g, '');
+    if(s.split('').reverse().join('')!==s) return "NO";
+    return answer;
+}
+
+let word="found7, time: study; Yduts; emit, 7Dnuof";
+console.log(sol(word));
+
